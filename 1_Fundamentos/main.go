dd8b37519090f3c1c82d1a86f2ebccc8d6447572
gabriel-albqr <gabrielg@petribusa.com.br>
2025-11-24 22:39:13 -0300

    Variáveis

--- 1_Fundamentos/main.go
+++ 1_Fundamentos/main.go
@@ -3,5 +3,12 @@ package main // define o programa principal
 import "fmt" // importa a biblioteca fmt para formatação de entrada e saída
 
 func main() { // função principal onde a execução do programa começa
+
+	var peso int = 70         // declara uma variável inteira chamada peso e atribui o valor 70
+	var altura float64 = 1.75 // declara uma variável float64 chamada altura e atribui o valor 1.75
+
+	fmt.Printf("Peso: %d kg\n", peso)      // imprime o peso formatado
+	fmt.Printf("Altura: %.2f m\n", altura) // imprime a altura formatada
+
 	fmt.Println("Hello, World!") // imprime "Hello, World!" no console
 }
