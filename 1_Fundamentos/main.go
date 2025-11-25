7d31ac32a32c6bbf128c6cba3e41f6bd755a7944
gabriel-albqr <gabrielg@petribusa.com.br>
2025-11-24 21:08:52 -0300

    hello world

---
+++ 1_Fundamentos/main.go
@@ -0,0 +1,7 @@
+package main // define o programa principal
+
+import "fmt" // importa a biblioteca fmt para formatação de entrada e saída
+
+func main() { // função principal onde a execução do programa começa
+	fmt.Println("Hello, World!") // imprime "Hello, World!" no console
+}
